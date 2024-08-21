276985c4c5c45fdd62b3f242839fbc37ea642ce5
EC2 Default User <ec2-user@ip-172-31-2-77.ap-northeast-1.compute.internal>
2024-08-21 13:50:08 +0000

    rename sweets_shop

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'home/top'
+  end
+  namespace :public do
+    get 'home/top'
+  end
+  devise_for :users, controllers: {
+    registrations: 'public/users/registrations',
+    sessions: 'public/users/sessions'
+  }
+  devise_for :admins, controllers: {
+    sessions: 'admin/admins/sessions'
+  }
+  
+  namespace :admin do
+    root "home#top"
+  end
+  
+  scope module: :public do
+    root "home#top"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
